b75d51581bc4a961faa21540b6af5b0ddc0a0fc3
darylhb28 <darylhornickbecker@gmail.com>
2025-02-05 18:19:42 -0500

    Add files via upload

---
+++ workshop.js
@@ -0,0 +1,22 @@
+// STEPS TO FOLLOW TO COMPLETE WORKSHOP
+// 1. Create a string for the user saying "You have received this message because you have been chosen to open an important vault. Here is the secret combination:
+// 2. Assign the first variable using an arithamtic operator to equal 10.
+// 3. Assign the second variable using an arithamtic operator to equal 40.
+// 4. Assign the third variable using an arithmatic operator to equal 39.
+// 5. Concatenate the string and the variables in a pop-up dialog box.
+
+// Step 1 - creating a string 
+const str = "You have received this message because you have been chosen to open an important vault. Here is the secret combination: "
+
+// Step 2 - assigning first variable using +
+const num1 = 4 + 6
+
+// Step 3 - assigning second variable using *
+const num2 = 10 * 4
+
+// Step 4 - assigning third variable using -
+const num3 = 40 - 1
+
+// Step 5 - concatenating string and varibale to display message with code, using template literals
+alert(`${str} ${num1} - ${num2} - ${num3}`)
+
